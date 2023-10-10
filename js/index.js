338117527bd0386ac947f6bed07c4e1f618476c0
reagan <reaganm746@gmail.com>
2023-10-10 11:51:00 +0300

    render to web page

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 //ensure the DOM is loaded first
+/*
 document.addEventListener('DOMContentLoaded',()=>{
   getCars();
 })
@@ -41,5 +42,45 @@ function getCars(){
   .catch(error => console.log('error', error));
 }
 getCars();
+*/
+document.addEventListener('DOMContentLoaded', () => {
+  getCars();
+});
 
- 
+const myHeaders = new Headers();
+myHeaders.append("X-Api-Key", "TwBqMEpJ57NJsOil94499w==Natjj0ssjnbeQb5x");
+
+const requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+};
+
+function renderCars(cars) {
+  const carListContainer = document.getElementById('carList');
+
+  if (cars && cars.length > 0) {
+    // Create an HTML list to display the cars
+    const ul = document.createElement('ul');
+
+    cars.forEach(car => {
+      const li = document.createElement('li');
+      li.textContent = `Make: ${car.make}, Model: ${car.model}`;
+      ul.appendChild(li);
+    });
+
+    // Clear any existing content in the container
+    carListContainer.innerHTML = '';
+
+    // Append the list to the container
+    carListContainer.appendChild(ul);
+  } else {
+    carListContainer.textContent = 'No cars found.';
+  }
+}
+
+function getCars() {
+  fetch("https://api.api-ninjas.com/v1/cars?limit=50&model=supra", requestOptions)
+    .then(response => response.json())
+    .then(result => renderCars(result)) // Call renderCars to display the data
+    .catch(error => console.log('error', error));
+}
