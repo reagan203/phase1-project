452e271e9cc8969bd505599491cec5f617fc8980
reagan <reaganm746@gmail.com>
2023-10-10 20:32:43 +0300

    functionality

--- js/index.js
+++ js/index.js
@@ -56,10 +56,37 @@ function renderCars(cars) {
 }
 
 function getCars() {
-  fetch("https://api.api-ninjas.com/v1/cars?limit=10&model=supra", requestOptions)
+  fetch("https://api.api-ninjas.com/v1/cars?limit=30&model=supra", requestOptions)
     .then(response => response.json())
     .then(result => renderCars(result))
     .catch(error => console.log('error', error)
 )}
+
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const commentText = document.getElementById("commentText");
+  const submitComment = document.getElementById("submitComment");
+  const commentList = document.getElementById("commentList");
+
+  submitComment.addEventListener("click", function () {
+      // text from the comment input
+      const text = commentText.value;
+
+      // Check if the input is not empty
+      if (text.trim() !== "") {
+          // Create a new comment element
+          const commentElement = document.createElement("div");
+          commentElement.className = "comment";
+          commentElement.textContent = text;
+
+          // Append the comment element to the comment list
+          commentList.appendChild(commentElement);
+
+          // Clear the comment input
+          commentText.value = "";
+      }
+  });
+});
+
 
 
